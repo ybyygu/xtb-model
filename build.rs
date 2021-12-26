6d98fa4a4d6555aadc6bd630ff43dcae3d2c6212
Wenping Guo <ybyygu@gmail.com>
2021-12-26 20:15:58 +0800

    Remove deprecated wrapper in C

--- build.rs
+++ build.rs
@@ -1,26 +1,10 @@
 // [[file:xtb.note::0b9b361d][0b9b361d]]
 use bindgen;
-use cc;
 
-use std::env;
 use std::path::PathBuf;
 
 fn main() {
     //	gcc -o test main.c -I include/xtb build/libxtb.a -lgfortran -lm -lblas -llapack
-    cc::Build::new()
-        .cpp(false)
-        .include("include")
-        // .flag("-lm")
-        // .flag("-lblas")
-        // .flag("-llapack")
-        // .flag("-lgfortran")
-        // .flag("-Lbuild")
-        // .flag("-lxtb")
-        // .flag("build/libxtb.a")
-        // local wrapper for exposing only API what I need
-        .file("wrapper.c")
-        .compile("xtbmodel");
-
     println!("cargo:rustc-link-search=lib");
     println!("cargo:rustc-link-lib=static=xtb");
     println!("cargo:rustc-link-lib=m");
@@ -29,13 +13,12 @@ fn main() {
     println!("cargo:rustc-link-lib=gfortran");
 
     let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
         .header("include/xtb.h")
         .rustfmt_bindings(true)
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
